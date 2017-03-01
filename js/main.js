21ccedb9cab2e8c4c8ce7488e1e71bea28c56d90
maxym.rekun <maxym.rekun@gmail.com>
2017-03-01 11:46:31 +0200

    first_test_commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  jQuery(document).ready(function($) {
+      $("#fullpage").fullpage(
+      {
+        sectionsColor:['#04A10E','#001F01','#04A10E'],
+        anchors:['block1','block2','block3'],
+        menu:'#menu',
+      }
+      );
+
+  })
